9e9c2e5e35c9e9428c5dede9eac210e7cc6e4ef9
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-06 19:11:21 -0500

    Update Dictionary.lua

--- Dictionary.lua
+++ Dictionary.lua
@@ -40,4 +40,109 @@ function DictionaryHelper:Watch(callback)
 	return Changed.Event;
 end
 
+function DictionaryHelper:GetSize()
+	local size = 0;
+	
+	for _ in pairs(self) do
+		size += 1;
+	end;
+	
+	return size;
+end
+
+function DictionaryHelper:Each(callback)
+	for index, value in pairs(self) do 
+		callback(index, value);
+	end;
+end
+
+function DictionaryHelper:Keys()
+	local keys = { };
+
+	for index, _ in pairs(self) do 
+		table.insert(keys, index);
+	end;
+
+	return keys;
+end
+
+function DictionaryHelper:Values()
+	local values = { };
+
+	for _, val in pairs(self) do 
+		table.insert(values, val);
+	end;
+
+	return values;
+end
+
+function DictionaryHelper:Find(Callback, ReturnIndex)
+	local tabValue;
+
+	for index, value in pairs(self) do 
+		local Result = Callback(index, value);
+
+		if not Result then 
+			continue;
+		end;
+
+		tabValue = ReturnIndex and index or value;
+		break;
+	end;
+
+	return tabValue;
+end
+
+function DictionaryHelper:Random()
+	local size = 0;
+
+	for _ in pairs(self) do
+		size += 1;
+	end;
+	
+	local keys = { };
+
+	for index, _ in pairs(self) do 
+		table.insert(keys, index);
+	end;
+	
+	local RandomIndex = math.random(1, size);
+	local PickedValue = keys[RandomIndex];
+	return RandomIndex, PickedValue;
+end
+
+function DictionaryHelper:FindValue(index)	
+	local function deepSearch(val)
+		local lookingThrough = if val then val else self;
+		
+		for key, value in pairs(lookingThrough) do		
+			if (rawequal(key, index)) then
+				return value;
+			end;
+
+			if (typeof(value) == "table") then
+				return deepSearch(value);
+			end;
+		end;
+	end;
+	
+	return deepSearch();
+end
+
+function DictionaryHelper:Map(modifier)
+	local map = {content = { }};
+
+	for index, value in pairs(self) do 
+		local str = modifier(index, value);
+		table.insert(map.content, str);
+	end;
+
+	function map.join(separator)
+		return table.concat(map.content, separator);
+	end;
+	
+	-- make more map functions later..?
+	return map;
+end
+
 return DictionaryHelper;
