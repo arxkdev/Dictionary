7335a1904159c120f44ad7631e4a2fa852d4816c
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-01-06 19:43:14 -0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,4 +1,13 @@
+local HttpService = game:GetService("HttpService");
 local Dictionary = require(script.Parent.Parent);
 
 -- Create a new dictionary
 local People = Dictionary.new();
+
+-- Watch the dictionary for changes
+People:Watch():Connect(function(key, value)
+    -- Print the key and value of the dictionary when it changes
+	print(
+        "Dictionary changed: " .. key .. " = " .. HttpService:JSONEncode(value)
+    );
+end);
